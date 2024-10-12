2850f257f12f283e46db2445132aa3c1dd4736b6
Akhilesh Thapliyal <thapliyalakhilesh29@gmail.com>
2024-10-12 06:57:48 +0530

    first commit of this project

---
+++ features/home.feature
@@ -0,0 +1,20 @@
+Feature: Cura Health Home page
+
+  Scenario: Verify the home page
+    Given Launch the Chrome Browser
+    When Pass the website URL in Browser
+    Then User must land to Website Home Page
+
+  Scenario: Verify the functionality of Appointment link button
+    Given Launch the Chrome Browser
+    When Pass the website URL in Browser
+    And Click on the Appointment Button
+    Then User should redirect to Appointment Page
+
+  Scenario: Verify the functionality of Menu Button
+    Given Launch the Chrome Browser
+    When Pass the website URL in Browser
+    And Click on the Menu icon
+    Then Menu bar  should open & have Home & Login Shortcut links
+
+
---
+++ features/steps/home.py
@@ -0,0 +1,46 @@
+from behave import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+@given(u'Launch the Chrome Browser')
+def step_impl(context):
+    context.driver = webdriver.Chrome()
+    context.driver.maximize_window()
+
+
+@when(u'Pass the website URL in Browser')
+def step_impl(context):
+    context.driver.get("https://katalon-demo-cura.herokuapp.com")
+
+
+@then(u'User must land to Website Home Page')
+def step_impl(context):
+    assert context.driver.title.__eq__("CURA Healthcare Service")
+    context.driver.quit()
+
+
+@when(u'Click on the Appointment Button')
+def step_impl(context):
+    context.driver.find_element(By.ID, "btn-make-appointment").click()
+
+
+@then(u'User should redirect to Appointment Page')
+def step_impl(context):
+    #assert context.driver.current_URL.__eq__("https://katalon-demo-cura.herokuapp.com/profile.php#login")
+    assert context.driver.find_element(By.XPATH, '//*[@id="login"]/div/div/div[1]/h2').text.__eq__("Login")
+    context.driver.quit()
+
+
+@when(u'Click on the Menu icon')
+def step_impl(context):
+    context.driver.find_element(By.ID, "menu-toggle").click()
+
+
+@then(u'Menu bar  should open & have Home & Login Shortcut links')
+def step_impl(context):
+    elements = context.driver.find_elements(By.CLASS_NAME, "active")
+    texts = [element.text for element in elements]
+    assert "Home" in texts
+    assert "Login" in texts
+    context.driver.quit()
